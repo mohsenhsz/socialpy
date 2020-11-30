e4493aef202f2c3b33577e7680735db94d40faf9
mohsen <moh3en.hz@gmail.com>
2020-11-30 13:11:43 +0330

    create profile for each user and extend the user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username + "'s" + ' Profile'
+
+
+"""
+kwargs--->{
+            'created':'Is a boolean that contains the post_save status',
+            'instance': 'Contains a copy of the saved object'
+               }
+               """
+@receiver(post_save, sender=User)     # post_save.connect(save_profile, sender=User)
+def save_profile(sender, **kwargs):
+    if kwargs['created']:
+        p1 = Profile(user=kwargs['instance'])
+        p1.save()
