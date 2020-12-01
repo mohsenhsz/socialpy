84332b5eb07a7660e4bb0ba87fef94b0f863428b
mohsen <moh3en.hz@gmail.com>
2020-12-01 18:21:27 +0330

    create comment model

--- core/models.py
+++ core/models.py
@@ -15,3 +15,15 @@ class Post(models.Model):
         from django.urls import reverse
         return reverse('post_details', args=[self.publish_date.year,
                         self.publish_date.month, self.publish_date.day, self.slug])
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE)
+    is_reply = models.BooleanField(default=False)
+    body = models.TextField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}-{self.post.content[:20]}'
